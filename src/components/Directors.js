b36c0ee0d0f72dc0da6edc6fdb691e11c1c6cc5d
+ <+>
2023-04-14 09:53:23 -0600

    router lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const showMovieDir = directors.map((director) => {
+    return (
+      <div key={director.name}>
+        <h3>{director.name}</h3>
+        <ul>{director.movies.map((movie) => <li key={movie}>{movie}</li>)}</ul>
+      </div>
+    )
+  })
+  return <div>
+    <h1>Directors Page</h1>
+    {showMovieDir}
+  </div>;
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div className="navbar">
+    <NavLink exact to='/'>Home
+    </NavLink>
+    <NavLink exact to='/movies'>Movies</NavLink>
+    <NavLink exact to='/directors'>Directors</NavLink>
+    <NavLink exact to='/actors'>Actors</NavLink>
+
+  </div>;
 }
 
 export default NavBar;
